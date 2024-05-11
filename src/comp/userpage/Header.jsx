e89499f3c89de34dc6ee482b9d1b01e67adeaf42
ssohyun0 <jsh0312033@gmail.com>
2024-05-12 00:33:08 +0900

    .
    .

--- src/comp/userpage/Header.jsx
+++ src/comp/userpage/Header.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
 
 function Header({ setSearchQuery }) {
   const navigate = useNavigate();
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const handleLogin = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   const goHome = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const handleSearch = (event) => {
@@ -21,22 +21,32 @@ function Header({ setSearchQuery }) {
 
   return (
     <header className="header">
-      <img src="logo.png" alt="Logo" className="logo" onClick={goHome}/>
+      <img src="logo.png" alt="Logo" className="logo" onClick={goHome} />
       <nav className="navigation">
         <a href="/club-news">동아리 소식</a>
         <a href="/security-news">보안 소식</a>
-        <a href="http://192.168.74.128:8000/" target="_blank" rel="noopener noreferrer">CTF</a> 
+        <a
+          href="http://192.168.74.128:8000/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CTF
+        </a>
       </nav>
       <div className="search-container">
         <input
           type="search"
           placeholder="웹사이트 검색..."
           value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type="submit" onClick={handleSearch}>검색</button>
+        <button type="submit" onClick={handleSearch}>
+          검색
+        </button>
       </div>
-      <button className="login-button" onClick={handleLogin}>로그인</button>
+      <button className="login-button" onClick={handleLogin}>
+        로그인
+      </button>
     </header>
   );
 }
